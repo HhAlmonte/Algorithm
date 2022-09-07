5ee4d81f5b2f03a094b6f2e9c20bafe5e19e29da
Hector Almonte <hbalmontess272@gmail.com>
2022-09-07 01:00:32 -0300

    Agregar archivos de proyecto.

---
+++ Algorithm/Day1_BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithm_I
+{
+    public class Day1_BinarySearch
+    {
+        public int Search(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            var index = 0;
+
+            if (nums.Contains(target))
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] == target)
+                    {
+                        index = Array.IndexOf(nums, nums[i]);
+                    }
+                }
+            }
+            else
+            {
+                index = -1;
+            }
+            
+            return index;
+        }
+    }
+}
---
+++ Algorithm/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Algorithm_I
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1_BinarySearch day1_BinarySearch = new Day1_BinarySearch();
+
+            int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+
+            var result = day1_BinarySearch.Search(nums, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
